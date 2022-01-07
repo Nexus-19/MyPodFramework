b2b6852a7ba6c78a073add022e87f214a062b7b9
Soumyattam Dey <soumyattamdey14@gmail.com>
2022-01-07 19:57:03 +0530

    Finished adding code to Framework

---
+++ MyPodFramework/MyPodFramework/ApiService.swift
@@ -0,0 +1,19 @@
+//
+//  ApiService.swift
+//  MyPodFramework
+//
+//  Created by Soumyattam Dey on 07/01/22.
+//
+
+import Foundation
+
+
+public class ApiService{
+    
+    private init() {}
+    
+    public static func getMyUrl()->String{
+        let myUrl="https://run.mocky.io/v3/8260aa5d-8af8-4cff-999e-6e81b217f0ba"
+        return myUrl
+    }
+}
